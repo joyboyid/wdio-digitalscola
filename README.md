2252eb285132a755b2783a8f70632fb9b2ba3b0f
joyboyid <ican.notfound@gmail.com>
2024-06-24 07:05:32 +0700

    complete project v2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # wdio-digitalscola
+
+**how to run:** npm run loginTest --isMobile
+**how to run:** npm run addItem --isMobile
+
+**Note:** "loginTest": "wdio run ./wdio.conf.js --spec ./test/specs/loginTest.js"
+**Note:** "addItem": "wdio run ./wdio.conf.js --spec ./test/specs/loginTest.js"
+
+**On package.json**
+
+*Running on Manjaro 22*
--- test/specs/addItem.js
+++ test/specs/addItem.js
@@ -1,19 +1,30 @@
 import assert from 'assert'
 
 describe('saucecode test', () => {
-    it('Login succsed', async () => {
+    it('Add Item succsed', async () => {
         await browser.url('https://www.saucedemo.com/')
 
         const usernameTextbox = await browser.$('#user-name')
         const passwordTextbox = await browser.$('#password')
         const loginButton = await browser.$('//input[@value="Login"]')
+        const  cartIcon = await browser.$('#shopping_cart_container')
+        const addItem = await browser.$('//*[@id="add-to-cart-sauce-labs-backpack"]')
+        const addItem1 = await browser.$('#add-to-cart-sauce-labs-bike-light')
+        const valItem = await browser.$('#cart_contents_container > div > div.cart_list')
 
         await usernameTextbox.setValue("standard_user")
         await passwordTextbox.setValue("secret_sauce")
         console.log(await loginButton.getValue())
         await loginButton.click()
         expect(browser).toHaveUrlContaining('dashboard')
-   
+
+        await expect(cartIcon).toBeDisplayed()
+        await addItem.click()
+        await addItem1.click()
+        await cartIcon.click()
+        await expect(valItem).toBeDisplayed()
+        expect(browser).toHaveUrlContaining('cart')
+        
         var pageUrl = browser.getUrl();
         assert.notEqual(pageUrl, 'https://www.saucedemo.com/');
     });
